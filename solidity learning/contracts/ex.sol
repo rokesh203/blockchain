c26b666e129e2eae406a0424597196834c836de5
rokesh203 <rokeshkrishnan203@gmail.com>
2025-08-13 07:22:01 +0530

    internalfuncwork

--- solidity learning/contracts/ex.sol
+++ solidity learning/contracts/ex.sol
@@ -4,28 +4,28 @@ pragma solidity ^0.8.30;
 
 contract BasicCalculator{
     uint256 public result;
-    function add(uint256 a,uint256 b)internal pure returns(uint256){
+    function add(uint256 a,uint256 b)internal {
         result = a+b;
     }
-    function sub(uint256 a,uint256 b)internal pure returns(uint256){
+    function sub(uint256 a,uint256 b)internal {
         result = a-b;
     }
 }
 
 contract AdvancedCalculator is BasicCalculator{
-    function multiply(uint256 a,uint256 b) internal pure returns(uint256){
+    function multiply(uint256 a,uint256 b) internal {
         result = a*b;
-
-    }
-    function divide(uint256 a,uint256 b) internal pure returns(uint256){
+        }
+    function divide(uint256 a,uint256 b) internal{
         result = a/b;
     }
-    function performOp(uint256 a,uint256 b,uint8 operation) public pure returns(uint256){
-        if(operation ==0 ) return add(a,b);
-        else if(operation == 1) return sub(a,b);
-        else if(operation == 2) return multiply(a,b);
-        else if(operation == 3) return divide(a,b);
-        else revert("Invalid operation")       
+    function performOp(uint256 a,uint256 b,uint8 operation) public  returns(uint256){
+        if(operation == 0 )  add(a,b);
+        else if(operation == 1)sub(a,b);
+        else if(operation == 2) multiply(a,b);
+        else if(operation == 3) divide(a,b);
+        else revert("Invalid operation");  
 
+        return result;
     }
 }
